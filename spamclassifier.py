bcfca09bc0fbc4a93bce4e87e90eb2cfc27d163b
valgardg <valgardo20@ru.is>
2023-09-06 23:31:05 +0000

    finished assignment with 2 bonus points

--- spamclassifier.py
+++ spamclassifier.py
@@ -1,7 +1,3 @@
-#
-# Simple solution to first (ungraded) assignment, created with ChatGPT in < 20 minutes
-#
-
 import os
 import numpy as np
 from sklearn.feature_extraction.text import CountVectorizer
@@ -38,12 +34,9 @@ def preprocess_text(text):
     words = [stemmer.stem(word) for word in words] # More Stemming
     # print("processed_text: ", ' '.join(words))
     return ' '.join(words)  # Convert the list of words to a string
-
-
 def load_spam_data(data_directory):
     data = []
     labels = []
-
     for filename in os.listdir(data_directory):
         if filename.startswith("spm"):
             labels.append(1)
@@ -54,7 +47,6 @@ def load_spam_data(data_directory):
             email_text = file.read()
             preprocessed_text = preprocess_text(email_text)
             data.append(preprocessed_text)
-
     return data, labels
 
 # Load the training data and labels
@@ -77,7 +69,8 @@ def run_comparison(vectorizer, train_data, train_labels, test_data, test_labels)
     models = {
         'Naive Bayes': MultinomialNB(),
         'Logistic Regression': LogisticRegression(C=0.1), # chosen for experiment 1.3
-        'k-NN': KNeighborsClassifier(n_neighbors=10) # chosen from experiment 1.2
+        'k-NN': KNeighborsClassifier(n_neighbors=10), # chosen from experiment 1.2
+        'Random Forest': RandomForestClassifier(n_estimators=100)
     }
     
     plt.figure(figsize=(10, 8))
@@ -118,44 +111,3 @@ def run_comparison(vectorizer, train_data, train_labels, test_data, test_labels)
     plt.show()
 
 run_comparison(vectorizer, train_data, train_labels, test_data, test_labels)
-
-
-# # Create a CountVectorizer with a maximum vocabulary size of 2000 words
-# vectorizer = CountVectorizer(max_features=2000)
-# X_train = vectorizer.fit_transform(train_data)
-# X_test = vectorizer.transform(test_data)
-
-# # Train a Multinomial Naive Bayes classifier
-# classifier = MultinomialNB()
-# classifier.fit(X_train, train_labels)
-
-# # Predict on the test data
-# test_predictions = classifier.predict(X_test)
-
-# # Evaluate the classifier
-# accuracy = accuracy_score(test_labels, test_predictions)
-# precision = precision_score(test_labels, test_predictions)
-# recall = recall_score(test_labels, test_predictions)
-# f1 = f1_score(test_labels, test_predictions)
-
-# print("Accuracy:", accuracy)
-# print("Precision:", precision)
-# print("Recall:", recall)
-# print("F1-score:", f1)
-
-# # Calculate ROC curve
-# fpr, tpr, thresholds = roc_curve(test_labels, classifier.predict_proba(X_test)[:, 1])
-# roc_auc = auc(fpr, tpr)
-# print("AUC-ROC:", roc_auc)
-
-# # Plot ROC curve
-# plt.figure()
-# plt.plot(fpr, tpr, color='darkorange', lw=2, label='ROC curve (area = %0.2f)' % roc_auc)
-# plt.plot([0, 1], [0, 1], color='navy', lw=2, linestyle='--')
-# plt.xlim([0.0, 1.0])
-# plt.ylim([0.0, 1.05])
-# plt.xlabel('False Positive Rate')
-# plt.ylabel('True Positive Rate')
-# plt.title('Receiver Operating Characteristic')
-# plt.legend(loc="lower right")
-# plt.show()
